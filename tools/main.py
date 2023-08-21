a7535f1d40bf73fbced283e9564f8cfb065e56fd
Laky64 <iraci.matteo@gmail.com>
2023-08-22 00:40:09 +0200

    Update example

--- tools/main.py
+++ tools/main.py
@@ -1,8 +1,8 @@
 import asyncio
+import logging
 
-# from ntgcalls import NTgCalls
+from ntgcalls import NTgCalls
 from pyrogram import Client, idle
-import webrtc
 from pyrogram.raw.functions.channels import GetFullChannel
 from pyrogram.raw.functions.phone import JoinGroupCall
 from pyrogram.raw.types import UpdateGroupCallConnection, Updates, DataJSON, InputChannel
@@ -10,26 +10,17 @@ from pyrogram.raw.types import UpdateGroupCallConnection, Updates, DataJSON, Inp
 api_id = 2799555
 api_hash = '47d66bbf0939d0ddf32caf8bad590ed7'
 
-#wrtc = NTgCalls()
+wrtc = NTgCalls()
 
 
 async def main():
-    client = Client('test', api_id, api_hash)
+    client = Client('test', api_id, api_hash, sleep_threshold=1)
 
     file_audio = "C:/Users/iraci/PycharmProjects/NativeTgCalls/tools/output.pcm"
-    pc = webrtc.RTCPeerConnection()
-    print("TEST")
-    # print(wrtc.createCall(file_audio))
-    # file_audio = "C:/Users/iraci/PycharmProjects/NativeTgCalls/tools/opus"
-    """ TESTING with browser.html
-    call_params = wrtc.createCall(file_audio)
-    wrtc.setRemoteCallParams("{}")
-    await idle()
-    """
 
-    """async with client:
+
+    async with client:
         call_params = wrtc.createCall(file_audio)
-        print(call_params)
         chat = await client.resolve_peer(-1001398466177)
         local_peer = await client.resolve_peer((await client.get_me()).id)
         input_call = (
@@ -46,15 +37,16 @@ async def main():
             JoinGroupCall(
                 call=input_call,
                 params=DataJSON(data=call_params),
+                video_stopped=False,
                 muted=False,
                 join_as=local_peer,
-            ),
+            ), sleep_threshold=0, retries=0
         )
         for update in result.updates:
             if isinstance(update, UpdateGroupCallConnection):
                 wrtc.setRemoteCallParams(update.params.data)
         print("Connected!")
         await idle()
-        print("Closed")"""
+        print("Closed")
 
 asyncio.new_event_loop().run_until_complete(main())
