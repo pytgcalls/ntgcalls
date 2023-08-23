aca4312c782059feda1d053a23f88fe93ca3d17a
Laky64 <iraci.matteo@gmail.com>
2023-08-23 16:20:35 +0200

    Added NtTgCalls client manager

--- ntgcalls/ntgcalls.cpp
+++ ntgcalls/ntgcalls.cpp
@@ -6,8 +6,52 @@
 
 namespace ntgcalls {
 
-    void NTgCalls::init(int64_t chatId) {
+    std::string NTgCalls::createCall(int64_t chatId, MediaDescription media) {
+        if (exists(chatId)) {
+            throw ConnectionError("Connection cannot be initialized more than once.");
+        }
+        connections[chatId] = Client();
+        return connections[chatId].init(media);
+    }
 
+    void NTgCalls::connect(int64_t chatId, std::string params) {
+        safeConnection(chatId).connect(params);
+    }
+
+    void NTgCalls::changeStream(int64_t chatId, MediaDescription media) {
+        safeConnection(chatId).changeStream(media);
+    }
+
+    void NTgCalls::pause(int64_t chatId) {
+        safeConnection(chatId).pause();
+    }
+
+    void NTgCalls::resume(int64_t chatId) {
+        safeConnection(chatId).resume();
+    }
+
+    void NTgCalls::mute(int64_t chatId) {
+        safeConnection(chatId).mute();
+    }
+
+    void NTgCalls::unmute(int64_t chatId) {
+        safeConnection(chatId).unmute();
+    }
+
+    void NTgCalls::stop(int64_t chatId) {
+        safeConnection(chatId).stop();
+    }
+
+    bool NTgCalls::exists(int64_t chatId) {
+        auto it = connections.find(chatId);
+        return it != connections.end();
+    }
+
+    Client NTgCalls::safeConnection(int64_t chatId) {
+        if (!exists(chatId)) {
+            throw ConnectionError("Connection with chat id \"" + std::to_string(chatId) + "\" not found");
+        }
+        return connections[chatId];
     }
 
 } // ntgcalls
--- ntgcalls/ntgcalls.hpp
+++ ntgcalls/ntgcalls.hpp
@@ -4,6 +4,7 @@
 
 #include <cstdint>
 #include "client.hpp"
+#include "models/media_description.hpp"
 
 namespace ntgcalls {
 
@@ -11,8 +12,26 @@ namespace ntgcalls {
     private:
         std::map<int64_t, Client> connections;
 
+        bool exists(int64_t chatId);
+
+        Client safeConnection(int64_t chatId);
+
     public:
-        void init(int64_t chatId);
+        std::string createCall(int64_t chatId, MediaDescription media);
+
+        void connect(int64_t chatId, std::string params);
+
+        void changeStream(int64_t chatId, MediaDescription media);
+
+        void pause(int64_t chatId);
+
+        void resume(int64_t chatId);
+
+        void mute(int64_t chatId);
+
+        void unmute(int64_t chatId);
+
+        void stop(int64_t chatId);
     };
 
 } // ntgcalls
