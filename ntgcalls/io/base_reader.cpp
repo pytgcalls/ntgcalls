61128690e2a0a5891d4977c77ae9a4740a8da07f
Laky64 <iraci.matteo@gmail.com>
2023-08-26 15:14:27 +0200

    Fixed memory leak with file reader

--- ntgcalls/io/base_reader.cpp
+++ ntgcalls/io/base_reader.cpp
@@ -9,7 +9,7 @@ namespace ntgcalls {
     wrtc::binary BaseReader::read(size_t size) {
         wrtc::binary res;
         auto promise = std::make_shared<std::promise<void>>();
-        if (!_eof) {
+        if (!_eof && nextBuffer.size() <= 10) {
             dispatchQueue.dispatch([this, promise, size] {
                 wrtc::binary tmpBuff = readInternal(size);
                 mtx.lock();
@@ -28,8 +28,8 @@ namespace ntgcalls {
         if (nextBuffer.empty() && !_eof) {
             promise->get_future().wait();
         }
-        res = nextBuffer[0];
         mtx.lock();
+        res = nextBuffer[0];
         nextBuffer.erase(nextBuffer.begin(), nextBuffer.begin() + 1);
         mtx.unlock();
         return res;
@@ -40,6 +40,6 @@ namespace ntgcalls {
     }
 
     bool BaseReader::eof() {
-        return _eof;
+        return _eof && nextBuffer.empty();
     }
 }
