032f183d20333cbbf88769ed8fefc5d18de13862
Laky64 <iraci.matteo@gmail.com>
2023-08-26 19:13:42 +0200

    Linux Compilation Issue Fix Attempt

--- cmake/Linux.cmake
+++ cmake/Linux.cmake
@@ -1,12 +1,18 @@
 target_compile_definitions(${target_name} PUBLIC
     WEBRTC_POSIX
     WEBRTC_LINUX
+    _LIBCPP_ABI_NAMESPACE=Cr
+    _LIBCPP_ABI_VERSION=2
+    _LIBCPP_DISABLE_AVAILABILITY
 )
 
 set_target_properties(${target_name} PROPERTIES POSITION_INDEPENDENT_CODE ON)
-
+target_compile_options(${target_name}
+    PRIVATE
+    "$<$<COMPILE_LANGUAGE:CXX>:-nostdinc++>"
+    "$<$<AND:$<COMPILE_LANGUAGE:CXX>,$<BOOL:LIBCXX_INCLUDE_DIR>>:-isystem${LIBCXX_INCLUDE_DIR}>"
+)
 target_link_libraries(${target_name} PRIVATE
-    X11
     dl
     rt
     Threads::Threads
