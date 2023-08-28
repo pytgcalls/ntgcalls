6893e158a93de77cc2f9d130736fcc23f70c271e
Laky64 <iraci.matteo@gmail.com>
2023-08-28 13:14:46 +0200

    Addition of LibCXX ABI Installer
    Implemented an installer for the LibCXX ABI (Application Binary Interface) library.

--- cmake/FindLibCXX.cmake
+++ cmake/FindLibCXX.cmake
@@ -1,15 +1,25 @@
-if(NOT EXISTS ${deps_loc}/libcxx AND LINUX)
-    GitClone(
-        URL https://chromium.googlesource.com/external/github.com/llvm/llvm-project/libcxx.git
-        COMMIT main
-        DIRECTORY ${deps_loc}/libcxx
-    )
-    GitFile(
-        URL https://chromium.googlesource.com/chromium/src/buildtools.git/+/refs/heads/main/third_party/libc++/__config_site
-        DIRECTORY ${deps_loc}/libcxx/include/__config_site
+if(LINUX)
+    if (NOT EXISTS ${deps_loc}/libcxx)
+        GitClone(
+            URL https://chromium.googlesource.com/external/github.com/llvm/llvm-project/libcxx.git
+            COMMIT main
+            DIRECTORY ${deps_loc}/libcxx
+        )
+        GitFile(
+            URL https://chromium.googlesource.com/chromium/src/buildtools.git/+/refs/heads/main/third_party/libc++/__config_site
+            DIRECTORY ${deps_loc}/libcxx/include/__config_site
+        )
+    endif ()
+    if (NOT EXISTS ${deps_loc}/libcxxabi)
+        GitClone(
+            URL https://chromium.googlesource.com/external/github.com/llvm/llvm-project/libcxxabi.git
+            COMMIT main
+            DIRECTORY ${deps_loc}/libcxxabi
+        )
+    endif ()
+    add_compile_options(
+        "$<$<COMPILE_LANGUAGE:CXX>:-nostdinc++>"
+        "$<$<AND:$<COMPILE_LANGUAGE:CXX>,$<BOOL:LIBCXX_INCLUDE_DIR>>:-isystem${deps_loc}/libcxx/include>"
+        "$<$<AND:$<COMPILE_LANGUAGE:CXX>,$<BOOL:LIBCXXABI_INCLUDE_DIR>>:-isystem${deps_loc}/libcxxabi/include>"
     )
 endif ()
-add_compile_options(
-    "$<$<COMPILE_LANGUAGE:CXX>:-nostdinc++>"
-    "$<$<AND:$<COMPILE_LANGUAGE:CXX>,$<BOOL:LIBCXX_INCLUDE_DIR>>:-isystem${deps_loc}/libcxx/include>"
-)
