f38b4b1d73bf698f65b1c49123325367772f816c
Laky64 <iraci.matteo@gmail.com>
2023-08-31 17:56:10 +0200

    Conducted cleanup of CMake Utilities.

--- cmake/FindLibCXX.cmake
+++ cmake/FindLibCXX.cmake
@@ -1,25 +1,27 @@
 if(LINUX)
-    if (NOT EXISTS ${deps_loc}/libcxx)
+    set(LIBCXX_INCLUDE ${deps_loc}/libcxx)
+    set(LIBCXXABI_INCLUDE ${deps_loc}/libcxxabi)
+    if (NOT EXISTS ${LIBCXX_INCLUDE})
         GitClone(
             URL https://chromium.googlesource.com/external/github.com/llvm/llvm-project/libcxx.git
             COMMIT main
-            DIRECTORY ${deps_loc}/libcxx
+            DIRECTORY ${LIBCXX_INCLUDE}
         )
         GitFile(
             URL https://chromium.googlesource.com/chromium/src/buildtools.git/+/refs/heads/main/third_party/libc++/__config_site
-            DIRECTORY ${deps_loc}/libcxx/include/__config_site
+            DIRECTORY ${LIBCXX_INCLUDE}/include/__config_site
         )
     endif ()
-    if (NOT EXISTS ${deps_loc}/libcxxabi)
+    if (NOT EXISTS ${LIBCXXABI_INCLUDE})
         GitClone(
             URL https://chromium.googlesource.com/external/github.com/llvm/llvm-project/libcxxabi.git
             COMMIT main
-            DIRECTORY ${deps_loc}/libcxxabi
+            DIRECTORY ${LIBCXXABI_INCLUDE}
         )
     endif ()
     add_compile_options(
         "$<$<COMPILE_LANGUAGE:CXX>:-nostdinc++>"
-        "$<$<AND:$<COMPILE_LANGUAGE:CXX>,$<BOOL:LIBCXX_INCLUDE_DIR>>:-isystem${deps_loc}/libcxx/include>"
-        "$<$<AND:$<COMPILE_LANGUAGE:CXX>,$<BOOL:LIBCXXABI_INCLUDE_DIR>>:-isystem${deps_loc}/libcxxabi/include>"
+        "$<$<AND:$<COMPILE_LANGUAGE:CXX>,$<BOOL:LIBCXX_INCLUDE_DIR>>:-isystem${LIBCXX_INCLUDE}/include>"
+        "$<$<AND:$<COMPILE_LANGUAGE:CXX>,$<BOOL:LIBCXXABI_INCLUDE_DIR>>:-isystem${LIBCXXABI_INCLUDE}/include>"
     )
 endif ()
