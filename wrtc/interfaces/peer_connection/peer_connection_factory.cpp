a7f6c31bd606f79c10a3731cf853060738fce3a9
Laky64 <iraci.matteo@gmail.com>
2023-08-31 17:57:11 +0200

    Removed AudioDeviceModule

--- wrtc/interfaces/peer_connection/peer_connection_factory.cpp
+++ wrtc/interfaces/peer_connection/peer_connection_factory.cpp
@@ -21,17 +21,13 @@ namespace wrtc {
         _networkThread = rtc::Thread::CreateWithSocketServer();
         _networkThread->SetName("network_thread", nullptr);
         RTC_CHECK(_networkThread->Start()) << "Failed to start thread";
-        if (!_audioDeviceModule) {
-            _taskQueueFactory = webrtc::CreateDefaultTaskQueueFactory();
-            _workerThread->BlockingCall([this] { CreateAudioDeviceModule_w(); });
-        }
 
         auto config = VideoFactoryConfig();
 
         if (!_factory) {
             _factory = webrtc::CreatePeerConnectionFactory(
                     _networkThread.get(), _workerThread.get(), _signalingThread.get(),
-                    _audioDeviceModule,
+                    nullptr,
                     webrtc::CreateBuiltinAudioEncoderFactory(),
                     webrtc::CreateBuiltinAudioDecoderFactory(),
                     config.CreateVideoEncoderFactory(),
@@ -47,23 +43,9 @@ namespace wrtc {
     PeerConnectionFactory::~PeerConnectionFactory() {
         _factory = nullptr;
 
-        if (_audioDeviceModule) {
-            _workerThread->BlockingCall([this] { DestroyAudioDeviceModule_w(); });
-        }
-
         _workerThread->Stop();
         _signalingThread->Stop();
         _networkThread->Stop();
-    }
-
-    void PeerConnectionFactory::CreateAudioDeviceModule_w() {
-        if (!_audioDeviceModule)
-            _audioDeviceModule = webrtc::NoMicrophone::Create(_taskQueueFactory.get());
-    }
-
-    void PeerConnectionFactory::DestroyAudioDeviceModule_w() {
-        if (_audioDeviceModule)
-            _audioDeviceModule = nullptr;
     }
 
     rtc::scoped_refptr<webrtc::PeerConnectionFactoryInterface> PeerConnectionFactory::factory() {
