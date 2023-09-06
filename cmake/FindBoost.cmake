eb0de69a202f9e69f428d4143d618d0d325a9249
Laky64 <iraci.matteo@gmail.com>
2023-09-06 11:42:57 +0200

    Fixed Boost building

--- cmake/FindBoost.cmake
+++ cmake/FindBoost.cmake
@@ -54,78 +54,81 @@ if(NOT DEFINED LAST_BOOST_LIBS OR
         NOT EXISTS ${BOOST_DIR} OR
         NOT EXISTS ${BOOST_WORKDIR} OR
         NOT EXISTS ${BOOST_ROOT})
-    if (EXISTS ${BOOST_WORKDIR})
-        message(STATUS "[BOOST] Cleaning up old Boost build...")
-        file(REMOVE_RECURSE ${BOOST_WORKDIR})
-    endif ()
 
-    set(BOOST_DOWNLOAD_DIR ${BOOST_DIR}/download)
-    set(BOOST_TAR ${BOOST_DOWNLOAD_DIR}/boost_${BOOST_REVISION_UNDERSCORE}.tar.gz)
-    if (NOT EXISTS ${BOOST_TAR})
-        message(STATUS "[BOOST] Downloading for ${BOOST_REVISION} revision")
-        file(MAKE_DIRECTORY ${BOOST_DOWNLOAD_DIR})
-        file(DOWNLOAD
-            https://boostorg.jfrog.io/artifactory/main/release/${BOOST_REVISION}/source/boost_${BOOST_REVISION_UNDERSCORE}.tar.gz
-            ${BOOST_TAR}
-            SHOW_PROGRESS
-            STATUS status
-            LOG log
-        )
-        list(GET status 0 status_code)
-        if(status_code EQUAL 0)
-            message(STATUS "[BOOST] Downloading... done")
-        else ()
-            file(REMOVE_RECURSE ${BOOST_WORKDIR})
-            file(REMOVE ${BOOST_TAR})
-            message(FATAL_ERROR "[BOOST] Error: downloading '${url}' failed
+    if (NOT EXISTS ${BOOST_WORKDIR})
+        set(BOOST_DOWNLOAD_DIR ${BOOST_DIR}/download)
+        set(BOOST_TAR ${BOOST_DOWNLOAD_DIR}/boost_${BOOST_REVISION_UNDERSCORE}.tar.gz)
+        if (NOT EXISTS ${BOOST_TAR})
+            message(STATUS "[BOOST] Downloading for ${BOOST_REVISION} revision")
+            file(MAKE_DIRECTORY ${BOOST_DOWNLOAD_DIR})
+            file(DOWNLOAD
+                https://boostorg.jfrog.io/artifactory/main/release/${BOOST_REVISION}/source/boost_${BOOST_REVISION_UNDERSCORE}.tar.gz
+                ${BOOST_TAR}
+                SHOW_PROGRESS
+                STATUS status
+                LOG log
+            )
+            list(GET status 0 status_code)
+            if(status_code EQUAL 0)
+                message(STATUS "[BOOST] Downloading... done")
+            else ()
+                file(REMOVE_RECURSE ${BOOST_WORKDIR})
+                file(REMOVE ${BOOST_TAR})
+                message(FATAL_ERROR "[BOOST] Error: downloading '${url}' failed
             status_code: ${status_code}
             status_string: ${status_string}
             log:
             --- LOG BEGIN ---
             ${log}
             --- LOG END ---"
-            )
+                )
+            endif ()
+        endif ()
+
+        message(STATUS "[BOOST] Extracting...
+        src='${BOOST_TAR}'
+        dst='${BOOST_WORKDIR}'"
+        )
+
+        if(NOT EXISTS "${BOOST_TAR}")
+            message(FATAL_ERROR "[BOOST] File to extract does not exist: '${BOOST_TAR}'")
+        endif()
+        execute_process(COMMAND ${CMAKE_COMMAND} -E tar xfz ${BOOST_TAR}
+            WORKING_DIRECTORY ${BOOST_DIR}
+            RESULT_VARIABLE rv
+            OUTPUT_QUIET
+            ERROR_QUIET
+        )
+        if(NOT rv EQUAL 0)
+            message(STATUS "[BOOST] Extracting... [error clean up]")
+            file(REMOVE_RECURSE "${BOOST_WORKDIR}")
+            file(REMOVE ${BOOST_TAR})
+            message(FATAL_ERROR "[BOOST] Extract of '${BOOST_TAR}' failed")
+        endif()
+
+        if (WINDOWS)
+            set(BOOTSTRAP_EXECUTABLE .\\bootstrap.bat)
+        else ()
+            set(BOOTSTRAP_EXECUTABLE ./bootstrap.sh)
+        endif ()
+
+        message(STATUS "[BOOST] Executing bootstrap...")
+        execute_process(COMMAND ${BOOTSTRAP_EXECUTABLE}
+            WORKING_DIRECTORY ${BOOST_WORKDIR}
+            RESULT_VARIABLE rv
+            OUTPUT_QUIET
+            ERROR_QUIET
+        )
+
+        if(NOT rv EQUAL 0)
+            file(REMOVE_RECURSE ${BOOST_WORKDIR})
+            message(FATAL_ERROR "[BOOST] Error while executing bootstrap, cleaning up")
         endif ()
     endif ()
 
-    message(STATUS "[BOOST] Extracting...
-        src='${BOOST_TAR}'
-        dst='${BOOST_WORKDIR}'"
-    )
-
-    if(NOT EXISTS "${BOOST_TAR}")
-        message(FATAL_ERROR "[BOOST] File to extract does not exist: '${BOOST_TAR}'")
-    endif()
-    execute_process(COMMAND ${CMAKE_COMMAND} -E tar xfz ${BOOST_TAR}
-        WORKING_DIRECTORY ${BOOST_DIR}
-        RESULT_VARIABLE rv
-        OUTPUT_QUIET
-        ERROR_QUIET
-    )
-    if(NOT rv EQUAL 0)
-        message(STATUS "[BOOST] Extracting... [error clean up]")
-        file(REMOVE_RECURSE "${BOOST_WORKDIR}")
-        file(REMOVE ${BOOST_TAR})
-        message(FATAL_ERROR "[BOOST] Extract of '${BOOST_TAR}' failed")
-    endif()
-
-    if (WINDOWS)
-        set(BOOTSTRAP_EXECUTABLE .\\bootstrap.bat)
-    else ()
-        set(BOOTSTRAP_EXECUTABLE ./bootstrap.sh)
-    endif ()
-
-    message(STATUS "[BOOST] Executing bootstrap...")
-    execute_process(COMMAND ${BOOTSTRAP_EXECUTABLE}
-        WORKING_DIRECTORY ${BOOST_WORKDIR}
-        RESULT_VARIABLE rv
-        OUTPUT_QUIET
-        ERROR_QUIET
-    )
-
-    if(NOT rv EQUAL 0)
-        file(REMOVE_RECURSE ${BOOST_WORKDIR})
-        message(FATAL_ERROR "[BOOST] Error while executing bootstrap, cleaning up")
+    if (EXISTS ${BOOST_ROOT})
+        message(STATUS "[BOOST] Cleaning up old Boost build...")
+        file(REMOVE_RECURSE ${BOOST_ROOT})
     endif ()
 
     if (NOT WINDOWS)
@@ -138,32 +141,32 @@ if(NOT DEFINED LAST_BOOST_LIBS OR
     string (REPLACE ";" " " BOOST_C_FLAGS "${BOOST_C_FLAGS}")
     string (REPLACE ";" " " BOOST_CXX_FLAGS "${BOOST_CXX_FLAGS}")
     set(BUILD_COMMAND
-            ${B2_EXECUTABLE}
-            install
-            -d+0
-            --prefix=${BOOST_ROOT}
-            ${BOOST_LIBS_OPTIONS}
-            --layout=system
-            --ignore-site-config
-            variant=release
-            cflags=${BOOST_C_FLAGS}
-            cxxflags=${BOOST_CXX_FLAGS}
-            toolset=${BOOST_TOOLSET}
-            visibility=${BOOST_VISIBILITY}
-            target-os=${BOOST_TARGET}
-            address-model=64
-            link=static
-            runtime-link=${BOOST_LINK}
-            threading=multi
-            architecture=${BOOST_ARCH}
+        ${B2_EXECUTABLE}
+        install
+        -d+0
+        --prefix=${BOOST_ROOT}
+        ${BOOST_LIBS_OPTIONS}
+        --layout=system
+        --ignore-site-config
+        variant=release
+        cflags=${BOOST_C_FLAGS}
+        cxxflags=${BOOST_CXX_FLAGS}
+        toolset=${BOOST_TOOLSET}
+        visibility=${BOOST_VISIBILITY}
+        target-os=${BOOST_TARGET}
+        address-model=64
+        link=static
+        runtime-link=${BOOST_LINK}
+        threading=multi
+        architecture=${BOOST_ARCH}
     )
     message(STATUS "[BOOST] Executing build process...")
     execute_process(COMMAND ${BUILD_COMMAND}
         WORKING_DIRECTORY ${BOOST_WORKDIR}
         RESULT_VARIABLE rv
-        ERROR_VARIABLE ev
+        OUTPUT_QUIET
+        ERROR_QUIET
     )
-    message(STATUS ${ev})
     if(NOT rv EQUAL 0)
         file(REMOVE_RECURSE ${BOOST_ROOT})
         string (REPLACE ";" " " BUILD_COMMAND "${BUILD_COMMAND}")
