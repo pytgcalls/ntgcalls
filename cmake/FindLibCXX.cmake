1240571223d166a7d2b92d2395c955603fa1b5e5
Laky64 <iraci.matteo@gmail.com>
2023-09-10 01:57:56 +0200

    LibC++ Segfault Fix (Shiguredo WebRTC Release Commit)

--- cmake/FindLibCXX.cmake
+++ cmake/FindLibCXX.cmake
@@ -1,24 +1,20 @@
 if(LINUX)
     set(LIBCXX_INCLUDE ${deps_loc}/libcxx)
     set(LIBCXXABI_INCLUDE ${deps_loc}/libcxxabi)
-    if (NOT EXISTS ${LIBCXX_INCLUDE})
-        GitClone(
-            URL https://chromium.googlesource.com/external/github.com/llvm/llvm-project/libcxx.git
-            COMMIT main
-            DIRECTORY ${LIBCXX_INCLUDE}
-        )
-        GitFile(
-            URL https://chromium.googlesource.com/chromium/src/buildtools.git/+/refs/heads/main/third_party/libc++/__config_site
-            DIRECTORY ${LIBCXX_INCLUDE}/include/__config_site
-        )
-    endif ()
-    if (NOT EXISTS ${LIBCXXABI_INCLUDE})
-        GitClone(
-            URL https://chromium.googlesource.com/external/github.com/llvm/llvm-project/libcxxabi.git
-            COMMIT main
-            DIRECTORY ${LIBCXXABI_INCLUDE}
-        )
-    endif ()
+    GitClone(
+        URL https://chromium.googlesource.com/external/github.com/llvm/llvm-project/libcxx.git
+        COMMIT ${LIBCXX_COMMIT}
+        DIRECTORY ${LIBCXX_INCLUDE}
+    )
+    GitFile(
+        URL https://chromium.googlesource.com/chromium/src/buildtools.git/+/refs/heads/main/third_party/libc++/__config_site
+        DIRECTORY ${LIBCXX_INCLUDE}/include/__config_site
+    )
+    GitClone(
+        URL https://chromium.googlesource.com/external/github.com/llvm/llvm-project/libcxxabi.git
+        COMMIT ${LIBCXX_ABI_COMMIT}
+        DIRECTORY ${LIBCXXABI_INCLUDE}
+    )
     add_compile_options(
         "$<$<COMPILE_LANGUAGE:CXX>:-nostdinc++>"
         "$<$<AND:$<COMPILE_LANGUAGE:CXX>,$<BOOL:LIBCXX_INCLUDE_DIR>>:-isystem${LIBCXX_INCLUDE}/include>"
