4c1c0469826a5bf990847f807a84dd667c130abb
Laky64 <iraci.matteo@gmail.com>
2024-09-15 17:28:08 +0200

    Code cleanup

--- wrtc/src/utils/java_context.cpp
+++ wrtc/src/utils/java_context.cpp
@@ -4,8 +4,6 @@
 
 #include "wrtc/utils/java_context.hpp"
 
-#include <rtc_base/logging.h>
-
 #ifdef IS_ANDROID
 #include <modules/utility/include/jvm_android.h>
 #include <sdk/android/native_api/jni/jvm.h>
@@ -15,17 +13,12 @@
 namespace wrtc {
     void* GetJNIEnv() {
 #ifdef IS_ANDROID
-        RTC_LOG(LS_ERROR) << "GetJNIEnv::1";
         void* env = webrtc::AttachCurrentThreadIfNeeded();
-        RTC_LOG(LS_ERROR) << "GetJNIEnv::2" << env;
         static bool jvm_initialized = false;
         if (!jvm_initialized) {
-            RTC_LOG(LS_ERROR) << "GetJNIEnv::3" << env;
             webrtc::JVM::Initialize(webrtc::jni::GetJVM());
-            RTC_LOG(LS_ERROR) << "GetJNIEnv::4" << env;
             jvm_initialized = true;
         }
-        RTC_LOG(LS_ERROR) << "GetJNIEnv::5" << env;
         return env;
 #else
         return nullptr;
