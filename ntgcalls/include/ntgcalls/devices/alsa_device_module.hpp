9d0c5003a58b857bdf1a42f6c0bd35ba40068bcb
Laky64 <iraci.matteo@gmail.com>
2024-09-22 02:36:06 +0200

    Added "ALSA" Devices List support

--- ntgcalls/include/ntgcalls/devices/alsa_device_module.hpp
+++ ntgcalls/include/ntgcalls/devices/alsa_device_module.hpp
@@ -7,6 +7,7 @@
 #ifdef IS_LINUX
 #include <alsa/asoundlib.h>
 #include <ntgcalls/devices/base_device_module.hpp>
+#include <ntgcalls/devices/device_info.hpp>
 
 namespace ntgcalls {
 
@@ -14,6 +15,7 @@ namespace ntgcalls {
         snd_pcm_format_t format = SND_PCM_FORMAT_S16_LE;
         snd_pcm_t *captureHandle{};
         snd_pcm_hw_params_t *hwParams{};
+        std::string deviceID;
 
     public:
         AlsaDeviceModule(const AudioDescription* desc, bool isCapture);
@@ -21,6 +23,8 @@ namespace ntgcalls {
         [[nodiscard]] bytes::unique_binary read(int64_t size) override;
 
         static bool isSupported();
+
+        static std::vector<DeviceInfo> getDevices();
 
         void close() override;
     };
--- ntgcalls/src/devices/alsa_device_module.cpp
+++ ntgcalls/src/devices/alsa_device_module.cpp
@@ -13,9 +13,14 @@
 LATESYM_GET(webrtc::adm_linux_alsa::AlsaSymbolTable, GetAlsaSymbolTable(), sym)
 
 namespace ntgcalls {
-    AlsaDeviceModule::AlsaDeviceModule(const AudioDescription* desc, const bool isCapture): BaseDeviceModule(desc) {
-        if (const auto err = LATE(snd_pcm_open)(&captureHandle, desc->input.c_str(), isCapture ? SND_PCM_STREAM_CAPTURE:SND_PCM_STREAM_PLAYBACK, 0); err < 0) {
-            throw MediaDeviceError("cannot open audio device " + desc->input + " (" + LATE(snd_strerror)(err) + ")");
+    AlsaDeviceModule::AlsaDeviceModule(const AudioDescription* desc, const bool isCapture): BaseDeviceModule(desc, isCapture) {
+        try {
+            deviceID = deviceMetadata["id"];
+        } catch (...) {
+            throw MediaDeviceError("Invalid device metadata");
+        }
+        if (const auto err = LATE(snd_pcm_open)(&captureHandle, deviceID.c_str(), isCapture ? SND_PCM_STREAM_CAPTURE:SND_PCM_STREAM_PLAYBACK, 0); err < 0) {
+            throw MediaDeviceError("cannot open audio device " + deviceID + " (" + LATE(snd_strerror)(err) + ")");
         }
         LATE(snd_pcm_hw_params_malloc)(&hwParams);
         if (const auto err = LATE(snd_pcm_hw_params_any)(captureHandle, hwParams); err < 0) {
@@ -38,7 +43,7 @@ namespace ntgcalls {
         }
         LATE(snd_pcm_hw_params_free)(hwParams);
         if (const auto err = LATE(snd_pcm_prepare)(captureHandle); err < 0) {
-            throw MediaDeviceError("cannot prepare audio interface for use (" + desc->input + " " + std::string(LATE(snd_strerror)(err)) + ")");
+            throw MediaDeviceError("cannot prepare audio interface for use (" + deviceID + " " + std::string(LATE(snd_strerror)(err)) + ")");
         }
     }
 
@@ -52,6 +57,47 @@ namespace ntgcalls {
 
     bool AlsaDeviceModule::isSupported() {
         return GetAlsaSymbolTable()->Load();
+    }
+
+    std::vector<DeviceInfo> AlsaDeviceModule::getDevices() {
+        int card = -1;
+        auto appendDevice = [](std::vector<DeviceInfo>& devices, const char* name, const char* desc, const bool isCapture) {
+            const json metadata = {
+                {"is_microphone", isCapture},
+                {"id", name},
+            };
+            devices.emplace_back(desc, metadata.dump());
+        };
+        std::vector<DeviceInfo> devices;
+        while (!LATE(snd_card_next)(&card) && card >= 0) {
+            char **hints;
+            if (const auto err = LATE(snd_device_name_hint)(card, "pcm", reinterpret_cast<void***>(&hints)) < 0) {
+                throw MediaDeviceError("Error getting device hints (" + std::string(LATE(snd_strerror)(err)) + ")");
+            }
+            char **n = hints;
+            while (*n != nullptr) {
+                char *name = LATE(snd_device_name_get_hint)(*n, "NAME");
+                char *desc = LATE(snd_device_name_get_hint)(*n, "DESC");
+                char *io = LATE(snd_device_name_get_hint)(*n, "IOID");
+                if (name && desc) {
+                    if (io) {
+                        appendDevice(devices, name, desc, strcmp(io, "Input") == 0);
+                    } else {
+                        appendDevice(devices, name, desc, true);
+                        appendDevice(devices, name, desc, false);
+                    }
+                }
+                if (name)
+                    free(name);
+                if (desc)
+                    free(desc);
+                if (io)
+                    free(io);
+                n++;
+            }
+            LATE(snd_device_name_free_hint)(reinterpret_cast<void**>(hints));
+        }
+        return devices;
     }
 
     void AlsaDeviceModule::close() {
