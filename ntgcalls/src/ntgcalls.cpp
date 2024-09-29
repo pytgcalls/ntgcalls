5386834c43f990a1baa9d540fb7f1ca2d1e2645f
Laky64 <iraci.matteo@gmail.com>
2024-09-29 14:26:58 +0200

    Fixed a missing variable export

--- ntgcalls/src/ntgcalls.cpp
+++ ntgcalls/src/ntgcalls.cpp
@@ -82,7 +82,7 @@ namespace ntgcalls {
     }
 
     ASYNC_RETURN(void) NTgCalls::createP2PCall(const int64_t userId, const MediaDescription& media) {
-        SMART_ASYNC(this, media)
+        SMART_ASYNC(this, media, userId)
         std::lock_guard lock(mutex);
         CHECK_AND_THROW_IF_EXISTS(userId)
         connections[userId] = std::make_shared<P2PCall>(updateThread.get());
