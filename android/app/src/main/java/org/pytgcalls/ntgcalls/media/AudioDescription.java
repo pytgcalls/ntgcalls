aa03cfb1a4b6f59f9b5d75883252244a9345e810
Laky64 <iraci.matteo@gmail.com>
2024-09-30 15:52:18 +0200

    Fixed missing space

--- android/app/src/main/java/org/pytgcalls/ntgcalls/media/AudioDescription.java
+++ android/app/src/main/java/org/pytgcalls/ntgcalls/media/AudioDescription.java
@@ -2,6 +2,7 @@ package org.pytgcalls.ntgcalls.media;
 
 public class AudioDescription extends BaseMediaDescription {
     public final int sampleRate, bitsPerSample, channelCount;
+
     public AudioDescription(@InputMode int flags, String input, int sampleRate, int bitsPerSample, int channelCount) {
         super(flags, input);
         this.sampleRate = sampleRate;
--- android/app/src/main/java/org/pytgcalls/ntgcalls/media/BaseMediaDescription.java
+++ android/app/src/main/java/org/pytgcalls/ntgcalls/media/BaseMediaDescription.java
@@ -3,6 +3,7 @@ package org.pytgcalls.ntgcalls.media;
 public class BaseMediaDescription {
     public final @InputMode int inputMode;
     public final String input;
+
     public BaseMediaDescription(@InputMode int inputMode, String input) {
         this.inputMode = inputMode;
         this.input = input;
