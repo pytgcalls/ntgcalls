240d6276fa5a5966cbe82b4ca0d58976b429ba99
Laky64 <iraci.matteo@gmail.com>
2024-10-03 16:07:41 +0200

    Added Abstract NativeNetworkInterface

---
+++ wrtc/include/wrtc/interfaces/native_network_interface.hpp
@@ -0,0 +1,98 @@
+//
+// Created by Laky64 on 01/10/24.
+//
+
+#pragma once
+
+#include <pc/dtls_transport.h>
+#include <api/scoped_refptr.h>
+#include <pc/dtls_srtp_transport.h>
+#include <p2p/base/dtls_transport.h>
+#include <rtc_base/rtc_certificate.h>
+#include <p2p/base/p2p_transport_channel.h>
+#include <p2p/client/basic_port_allocator.h>
+#include <wrtc/models/peer_ice_parameters.hpp>
+#include <wrtc/interfaces/network_interface.hpp>
+#include <wrtc/interfaces/media/channel_manager.hpp>
+#include <wrtc/interfaces/sctp_data_channel_provider_interface_impl.hpp>
+
+#include <wrtc/interfaces/media/channels/outgoing_audio_channel.hpp>
+#include <wrtc/interfaces/media/channels/outgoing_video_channel.hpp>
+
+
+namespace wrtc {
+
+    class NativeNetworkInterface:  public sigslot::has_slots<>, public NetworkInterface {
+        std::unique_ptr<cricket::BasicPortAllocator> portAllocator;
+        rtc::scoped_refptr<rtc::RTCCertificate> localCertificate;
+        std::unique_ptr<webrtc::AsyncDnsResolverFactoryInterface> asyncResolverFactory;
+
+        void DtlsReadyToSend(bool isReadyToSend);
+
+        void resetDtlsSrtpTransport();
+
+        void OnTransportWritableState_n(rtc::PacketTransportInternal*);
+
+        void OnTransportReceivingState_n(rtc::PacketTransportInternal*);
+
+        void UpdateAggregateStates_n();
+
+        void transportStateChanged(cricket::IceTransportInternal *transport);
+
+    protected:
+        std::unique_ptr<webrtc::Call> call;
+        LocalVideoAdapter videoSink;
+        webrtc::LocalAudioSinkAdapter audioSink;
+        std::unique_ptr<ChannelManager> channelManager;
+        std::unique_ptr<OutgoingAudioChannel> audioChannel;
+        std::unique_ptr<OutgoingVideoChannel> videoChannel;
+        PeerIceParameters localParameters, remoteParameters;
+        std::unique_ptr<cricket::DtlsTransport> dtlsTransport;
+        std::unique_ptr<webrtc::DtlsSrtpTransport> dtlsSrtpTransport;
+        std::unique_ptr<cricket::P2PTransportChannel> transportChannel;
+        std::unique_ptr<SctpDataChannelProviderInterfaceImpl> dataChannelInterface;
+        bool connected = false, failed = false;
+
+        virtual bool supportsPacketSending() const = 0;
+
+        virtual std::pair<cricket::ServerAddresses, std::vector<cricket::RelayServerConfig>> getStunAndTurnServers() = 0;
+
+        virtual cricket::RelayPortFactoryInterface* getRelayPortFactory() = 0;
+
+        virtual void setPortAllocatorFlags(cricket::BasicPortAllocator* portAllocator) = 0;
+
+        virtual int getRegatherOnFailedNetworksInterval() = 0;
+
+        virtual bool getCustomParameterBool(const std::string& name) const = 0;
+
+        virtual bool supportsRenomination() const = 0;
+
+        virtual cricket::IceRole iceRole() const = 0;
+
+        virtual cricket::IceMode iceMode() const = 0;
+
+        virtual int candidatePoolSize() const = 0;
+
+        virtual std::optional<rtc::SSLRole> dtlsRole() const = 0;
+
+        virtual void registerTransportCallbacks(cricket::P2PTransportChannel* transportChannel) = 0;
+
+        virtual void stateUpdated(bool isConnected) = 0;
+
+        virtual void start() = 0;
+
+        void close() override;
+
+    public:
+        NativeNetworkInterface();
+
+        PeerIceParameters localIceParameters();
+
+        std::unique_ptr<rtc::SSLFingerprint> localFingerprint() const;
+
+        void sendDataChannelMessage(const bytes::binary& data) const override;
+
+        static webrtc::CryptoOptions getDefaultCryptoOptions();
+    };
+
+} // wrtc
---
+++ wrtc/src/interfaces/native_network_interface.cpp
@@ -0,0 +1,208 @@
+//
+// Created by Laky64 on 01/10/24.
+//
+
+#include <p2p/base/basic_async_resolver_factory.h>
+#include <p2p/base/p2p_constants.h>
+#include <p2p/client/basic_port_allocator.h>
+#include <pc/media_factory.h>
+#include <rtc_base/crypto_random.h>
+#include <rtc_base/rtc_certificate_generator.h>
+#include <wrtc/interfaces/native_network_interface.hpp>
+
+namespace wrtc {
+    NativeNetworkInterface::NativeNetworkInterface() {
+        networkThread()->PostTask([this] {
+            localParameters = PeerIceParameters(
+                rtc::CreateRandomString(cricket::ICE_UFRAG_LENGTH),
+                rtc::CreateRandomString(cricket::ICE_PWD_LENGTH),
+                true
+            );
+            localCertificate = rtc::RTCCertificateGenerator::GenerateCertificate(
+                rtc::KeyParams(rtc::KT_ECDSA),
+                absl::nullopt
+            );
+            asyncResolverFactory = std::make_unique<webrtc::BasicAsyncDnsResolverFactory>();
+            dtlsSrtpTransport = std::make_unique<webrtc::DtlsSrtpTransport>(true, factory->fieldTrials());
+            dtlsSrtpTransport->SetDtlsTransports(nullptr, nullptr);
+            dtlsSrtpTransport->SetActiveResetSrtpParams(false);
+            dtlsSrtpTransport->SubscribeReadyToSend(this, [this](const bool readyToSend) {
+                DtlsReadyToSend(readyToSend);
+            });
+            dtlsSrtpTransport->SubscribeRtcpPacketReceived(this, [this](const rtc::CopyOnWriteBuffer* packet, int64_t) {
+               workerThread()->PostTask([this, packet = *packet] {
+                   call->Receiver()->DeliverRtcpPacket(packet);
+               });
+            });
+            if (supportsPacketSending()) {
+                dtlsSrtpTransport->SubscribeSentPacket(this, [this](const rtc::SentPacket &packet) {
+                    call->OnSentPacket(packet);
+                });
+            }
+            resetDtlsSrtpTransport();
+        });
+        channelManager = std::make_unique<ChannelManager>(
+            factory->mediaEngine(),
+            workerThread(),
+            networkThread(),
+            signalingThread()
+        );
+        workerThread()->BlockingCall([&] {
+            webrtc::CallConfig callConfig(factory->environment(), networkThread());
+            callConfig.audio_state = factory->mediaEngine()->voice().GetAudioState();
+            call = factory->mediaFactory()->CreateCall(callConfig);
+        });
+    }
+
+    void NativeNetworkInterface::DtlsReadyToSend(const bool isReadyToSend) {
+        UpdateAggregateStates_n();
+
+        if (isReadyToSend) {
+            networkThread()->PostTask([this] {
+                UpdateAggregateStates_n();
+            });
+        }
+    }
+
+    void NativeNetworkInterface::UpdateAggregateStates_n() {
+        const auto state = transportChannel->GetIceTransportState();
+        bool isConnected = false;
+        switch (state) {
+        case webrtc::IceTransportState::kConnected:
+        case webrtc::IceTransportState::kCompleted:
+            isConnected = true;
+            break;
+        default:
+            break;
+        }
+        if (!dtlsSrtpTransport->IsWritable(false)) {
+            isConnected = false;
+        }
+        if (connected != isConnected) {
+            connected = isConnected;
+            stateUpdated(isConnected);
+            if (dataChannelInterface) {
+                dataChannelInterface->updateIsConnected(isConnected);
+            }
+        }
+    }
+
+    void NativeNetworkInterface::resetDtlsSrtpTransport() {
+        portAllocator = std::make_unique<cricket::BasicPortAllocator>(
+            factory->networkManager(),
+            factory->socketFactory(),
+            nullptr,
+            getRelayPortFactory()
+        );
+        setPortAllocatorFlags(portAllocator.get());
+        portAllocator->Initialize();
+        auto [stunServers, turnServers] = getStunAndTurnServers();
+        portAllocator->SetConfiguration(stunServers, turnServers, candidatePoolSize(), webrtc::NO_PRUNE);
+
+        webrtc::IceTransportInit iceTransportInit;
+        iceTransportInit.set_port_allocator(portAllocator.get());
+        iceTransportInit.set_async_dns_resolver_factory(asyncResolverFactory.get());
+        transportChannel = cricket::P2PTransportChannel::Create("transport", 0, std::move(iceTransportInit));
+
+        cricket::IceConfig iceConfig;
+        iceConfig.continual_gathering_policy = cricket::GATHER_CONTINUALLY;
+        iceConfig.prioritize_most_likely_candidate_pairs = true;
+        iceConfig.regather_on_failed_networks_interval = getRegatherOnFailedNetworksInterval();
+        if (getCustomParameterBool("network_skip_initial_ping")) {
+            iceConfig.presume_writable_when_fully_relayed = true;
+        }
+        transportChannel->SetIceConfig(iceConfig);
+
+        const cricket::IceParameters localIceParameters(
+            localParameters.ufrag,
+            localParameters.pwd,
+            supportsRenomination()
+        );
+        transportChannel->SetIceParameters(localIceParameters);
+        transportChannel->SetIceRole(iceRole());
+        transportChannel->SignalIceTransportStateChanged.connect(this, &NativeNetworkInterface::transportStateChanged);
+        registerTransportCallbacks(transportChannel.get());
+
+        dtlsTransport = std::make_unique<cricket::DtlsTransport>(transportChannel.get(), getDefaultCryptoOptions(), nullptr);
+        dtlsTransport->SignalWritableState.connect(this, &NativeNetworkInterface::OnTransportWritableState_n);
+        dtlsTransport->SignalReceivingState.connect(this, &NativeNetworkInterface::OnTransportReceivingState_n);
+        if (const auto role = dtlsRole(); role.has_value()) {
+            dtlsTransport->SetDtlsRole(role.value());
+        }
+        dtlsTransport->SetLocalCertificate(localCertificate);
+        dtlsSrtpTransport->SetDtlsTransports(dtlsTransport.get(), nullptr);
+    }
+
+
+    void NativeNetworkInterface::transportStateChanged(cricket::IceTransportInternal*) {
+        UpdateAggregateStates_n();
+    }
+
+    webrtc::CryptoOptions NativeNetworkInterface::getDefaultCryptoOptions() {
+        auto options = webrtc::CryptoOptions();
+        options.srtp.enable_aes128_sha1_80_crypto_cipher = true;
+        options.srtp.enable_gcm_crypto_suites = true;
+        return options;
+    }
+
+    void NativeNetworkInterface::OnTransportWritableState_n(rtc::PacketTransportInternal*) {
+        assert(networkThread()->IsCurrent());
+        UpdateAggregateStates_n();
+    }
+
+    void NativeNetworkInterface::OnTransportReceivingState_n(rtc::PacketTransportInternal*){
+        assert(networkThread()->IsCurrent());
+        UpdateAggregateStates_n();
+    }
+
+    std::unique_ptr<rtc::SSLFingerprint> NativeNetworkInterface::localFingerprint() const {
+        const auto certificate = localCertificate;
+        if (!certificate) {
+            return nullptr;
+        }
+        return rtc::SSLFingerprint::CreateFromCertificate(*certificate);
+    }
+
+    void NativeNetworkInterface::close() {
+        channelManager = nullptr;
+        if (factory) {
+            workerThread()->BlockingCall([&] {
+                call = nullptr;
+            });
+        }
+        if (factory) {
+            networkThread()->BlockingCall([&] {
+                if (transportChannel) {
+                    transportChannel->SignalCandidateGathered.disconnect(this);
+                    transportChannel->SignalIceTransportStateChanged.disconnect(this);
+                    transportChannel->SignalNetworkRouteChanged.disconnect(this);
+                }
+                dataChannelInterface = nullptr;
+                if (dtlsTransport) {
+                    dtlsTransport->SignalWritableState.disconnect(this);
+                    dtlsTransport->SignalReceivingState.disconnect(this);
+                }
+                if (dtlsSrtpTransport) {
+                    dtlsSrtpTransport->UnsubscribeSentPacket(this);
+                    dtlsSrtpTransport->SetDtlsTransports(nullptr, nullptr);
+                }
+                dtlsTransport = nullptr;
+                transportChannel = nullptr;
+                portAllocator = nullptr;
+            });
+        }
+        NetworkInterface::close();
+    }
+
+    PeerIceParameters NativeNetworkInterface::localIceParameters() {
+        return localParameters;
+    }
+
+    void NativeNetworkInterface::sendDataChannelMessage(const bytes::binary& data) const {
+        networkThread()->PostTask([this, data] {
+            if (dataChannelInterface) {
+                dataChannelInterface->sendDataChannelMessage(data);
+            }
+        });
+    }
+} // wrtc
