fee96dc3fbbb4a498d87e77ae2c304fe284ae9d9
Laky64 <iraci.matteo@gmail.com>
2024-10-06 21:24:30 +0200

    Added IncomingAudioChannel

---
+++ wrtc/include/wrtc/interfaces/media/channels/incoming_audio_channel.hpp
@@ -0,0 +1,39 @@
+//
+// Created by Laky64 on 03/10/24.
+//
+
+#pragma once
+#include <call/call.h>
+#include <pc/dtls_srtp_transport.h>
+
+#include <wrtc/models/media_content.hpp>
+#include <wrtc/interfaces/media/channel_manager.hpp>
+
+namespace wrtc {
+
+    class IncomingAudioChannel final : public sigslot::has_slots<> {
+        uint32_t _ssrc = 0;
+        std::unique_ptr<cricket::VoiceChannel> channel;
+        rtc::Thread* workerThread;
+        rtc::Thread* networkThread;
+        int64_t activityTimestamp = 0;
+
+    public:
+        IncomingAudioChannel(
+            webrtc::Call* call,
+            ChannelManager *channelManager,
+            webrtc::RtpTransport* rtpTransport,
+            const MediaContent& mediaContent,
+            rtc::Thread *workerThread,
+            rtc::Thread* networkThread,
+            const std::function<void(const webrtc::AudioSinkInterface::Data&)>& dataCallback
+        );
+
+        ~IncomingAudioChannel() override;
+
+        void updateActivity();
+
+        [[nodiscard]] int64_t getActivity() const;
+    };
+
+} // wrtc
