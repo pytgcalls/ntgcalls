e6d5c859beb75012f131a144cd591b0d0ac86c50
Laky64 <iraci.matteo@gmail.com>
2024-10-08 13:56:56 +0200

    Fixed missing packet demuxing for DtlsSrtpTransport

--- wrtc/src/interfaces/wrapped_dtls_srtp_transport.cpp
+++ wrtc/src/interfaces/wrapped_dtls_srtp_transport.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <wrtc/interfaces/wrapped_dtls_srtp_transport.hpp>
+#include <modules/rtp_rtcp/source/rtp_util.h>
 
 namespace wrtc {
     WrappedDtlsSrtpTransport::WrappedDtlsSrtpTransport(
@@ -14,15 +15,36 @@ namespace wrtc {
     }
 
     void WrappedDtlsSrtpTransport::OnRtpPacketReceived(const rtc::ReceivedPacket& packet) {
+        if (!IsSrtpActive()) {
+            RTC_LOG(LS_WARNING) << "Inactive SRTP transport received an RTP packet. Drop it.";
+            return;
+        }
+
         rtc::CopyOnWriteBuffer payload(packet.payload());
+        char* data = payload.MutableData<char>();
+        int len = rtc::checked_cast<int>(payload.size());
+        if (!UnprotectRtp(data, len, &len)) {
+            if (decryptionFailureCount % 100 == 0) {
+                RTC_LOG(LS_ERROR) << "Failed to unprotect RTP packet: size=" << len
+                                  << ", seqnum=" << webrtc::ParseRtpSequenceNumber(payload)
+                                  << ", SSRC=" << webrtc::ParseRtpSsrc(payload)
+                                  << ", previous failure count: "
+                                  << decryptionFailureCount;
+            }
+            ++decryptionFailureCount;
+            return;
+        }
+        payload.SetSize(len);
 
         webrtc::RtpPacketReceived parsedPacket(&headerExtensionMap);
         parsedPacket.set_arrival_time(packet.arrival_time().value_or(webrtc::Timestamp::MinusInfinity()));
         parsedPacket.set_ecn(packet.ecn());
 
-        if (parsedPacket.Parse(std::move(payload))) {
+        if (parsedPacket.Parse(payload)) {
             (void) rtpPacketCallback(parsedPacket);
         }
+
+        DemuxPacket(payload, packet.arrival_time().value_or(webrtc::Timestamp::MinusInfinity()), packet.ecn());
     }
 
     void WrappedDtlsSrtpTransport::UpdateRtpHeaderExtensionMap(const cricket::RtpHeaderExtensions& headerExtensions) {
