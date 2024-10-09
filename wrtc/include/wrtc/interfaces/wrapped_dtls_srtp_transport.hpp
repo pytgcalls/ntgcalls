545189aab394559df36fbf9d8d740b0fe7db7a03
Laky64 <iraci.matteo@gmail.com>
2024-10-09 19:14:51 +0200

    Switch to forced accessing to private methods

--- wrtc/include/wrtc/interfaces/wrapped_dtls_srtp_transport.hpp
+++ wrtc/include/wrtc/interfaces/wrapped_dtls_srtp_transport.hpp
@@ -11,6 +11,7 @@ namespace wrtc {
     class WrappedDtlsSrtpTransport final : public webrtc::DtlsSrtpTransport {
         webrtc::RtpHeaderExtensionMap headerExtensionMap;
         synchronized_callback<webrtc::RtpPacketReceived> rtpPacketCallback;
+        int decryptionFailureCount = 0;
 
     public:
         WrappedDtlsSrtpTransport(
@@ -18,6 +19,8 @@ namespace wrtc {
             const webrtc::FieldTrialsView& field_trials,
             const std::function<void(webrtc::RtpPacketReceived)>& callback
         );
+
+        void OnRtpPacketReceived(const rtc::ReceivedPacket& packet) override;
 
         void UpdateRtpHeaderExtensionMap(const cricket::RtpHeaderExtensions& headerExtensions) override;
     };
--- wrtc/src/interfaces/wrapped_dtls_srtp_transport.cpp
+++ wrtc/src/interfaces/wrapped_dtls_srtp_transport.cpp
@@ -6,28 +6,58 @@
 #include <modules/rtp_rtcp/source/rtp_util.h>
 
 namespace wrtc {
+    template <typename Tag, typename Tag::pfn_t pfn>
+    struct tag_bind_pfn {
+        friend constexpr typename Tag::pfn_t pfn_of(Tag) { return pfn; }
+    };
+
+    struct tag_SrtpTransport_UnprotectRtp {
+        using pfn_t = bool (webrtc::SrtpTransport::*)(void* data, int in_len, int* out_len);
+        friend constexpr pfn_t pfn_of(tag_SrtpTransport_UnprotectRtp);
+    };
+    template struct tag_bind_pfn<tag_SrtpTransport_UnprotectRtp, &webrtc::SrtpTransport::UnprotectRtp>;
+
+    inline static constexpr auto c_pfn_SrtpTransport_UnprotectRtp = pfn_of(tag_SrtpTransport_UnprotectRtp{});
+
     WrappedDtlsSrtpTransport::WrappedDtlsSrtpTransport(
         const bool rtcpMuxEnabled,
         const webrtc::FieldTrialsView& field_trials,
         const std::function<void(webrtc::RtpPacketReceived)>& callback
     ): DtlsSrtpTransport(rtcpMuxEnabled, field_trials) {
         rtpPacketCallback = callback;
+    }
 
-        SubscribeReadyToSend(this, [this](const bool readyToSend) {
-            if (readyToSend) {
-                rtp_packet_transport()->RegisterReceivedPacketCallback(this, [this](rtc::PacketTransportInternal*, const rtc::ReceivedPacket& packet) {
-                    const rtc::CopyOnWriteBuffer payload(packet.payload());
+    void WrappedDtlsSrtpTransport::OnRtpPacketReceived(const rtc::ReceivedPacket& packet) {
+        if (!IsSrtpActive()) {
+            RTC_LOG(LS_WARNING) << "Inactive SRTP transport received an RTP packet. Drop it.";
+            return;
+        }
 
-                    webrtc::RtpPacketReceived parsedPacket(&headerExtensionMap);
-                    parsedPacket.set_arrival_time(packet.arrival_time().value_or(webrtc::Timestamp::MinusInfinity()));
-                    parsedPacket.set_ecn(packet.ecn());
-
-                    if (parsedPacket.Parse(payload)) {
-                        (void) rtpPacketCallback(parsedPacket);
-                    }
-                });
+        rtc::CopyOnWriteBuffer payload(packet.payload());
+        char* data = payload.MutableData<char>();
+        int len = rtc::checked_cast<int>(payload.size());
+        if (!(this->*c_pfn_SrtpTransport_UnprotectRtp)(data, len, &len)) {
+            if (decryptionFailureCount % 100 == 0) {
+                RTC_LOG(LS_ERROR) << "Failed to unprotect RTP packet: size=" << len
+                                  << ", seqnum=" << webrtc::ParseRtpSequenceNumber(payload)
+                                  << ", SSRC=" << webrtc::ParseRtpSsrc(payload)
+                                  << ", previous failure count: "
+                                  << decryptionFailureCount;
             }
-        });
+            ++decryptionFailureCount;
+            return;
+        }
+        payload.SetSize(len);
+
+        webrtc::RtpPacketReceived parsedPacket(&headerExtensionMap);
+        parsedPacket.set_arrival_time(packet.arrival_time().value_or(webrtc::Timestamp::MinusInfinity()));
+        parsedPacket.set_ecn(packet.ecn());
+
+        if (parsedPacket.Parse(payload)) {
+            (void) rtpPacketCallback(parsedPacket);
+        }
+
+        DemuxPacket(payload, packet.arrival_time().value_or(webrtc::Timestamp::MinusInfinity()), packet.ecn());
     }
 
     void WrappedDtlsSrtpTransport::UpdateRtpHeaderExtensionMap(const cricket::RtpHeaderExtensions& headerExtensions) {
