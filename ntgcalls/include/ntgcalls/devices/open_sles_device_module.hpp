5e831c1b0b30c2709214b5e5439b357fe73d8700
Laky64 <iraci.matteo@gmail.com>
2024-10-10 00:57:47 +0200

    Audio Playback support for OpenSLES Module

--- ntgcalls/include/ntgcalls/devices/open_sles_device_module.hpp
+++ ntgcalls/include/ntgcalls/devices/open_sles_device_module.hpp
@@ -11,11 +11,12 @@
 #include <ntgcalls/io/base_reader.hpp>
 #include <ntgcalls/devices/base_device_module.hpp>
 #include <sdk/android/src/jni/audio_device/opensles_common.h>
+#include <ntgcalls/io/audio_mixer.hpp>
 
 namespace ntgcalls {
     using namespace webrtc::jni;
 
-    class OpenSLESDeviceModule final: public BaseDeviceModule, public BaseReader {
+    class OpenSLESDeviceModule final: public BaseDeviceModule, public BaseReader, public AudioMixer {
         static constexpr int kNumOfOpenSLESBuffers = 2;
         int bufferIndex = 0;
         std::unique_ptr<bytes::unique_binary[]> audioBuffers;
@@ -23,17 +24,29 @@ namespace ntgcalls {
         SLEngineItf engine = nullptr;
         rtc::scoped_refptr<OpenSLEngineManager> engineManager;
         SLDataFormat_PCM pcmFormat{};
-        ScopedSLObjectItf recorderObject;
+        ScopedSLObjectItf deviceObject;
+        std::queue<bytes::unique_binary> queue;
+
         SLRecordItf recorder = nullptr;
+        SLPlayItf player = nullptr;
         SLAndroidSimpleBufferQueueItf simpleBufferQueue = nullptr;
+        ScopedSLObjectItf outputMix;
 
         void initRecording();
 
+        void initPlayback();
+
         static void SimpleBufferQueueCallback(SLAndroidSimpleBufferQueueItf, void* context);
+
+        void registerQueueCallback();
 
         [[nodiscard]] SLuint32 GetRecordState() const;
 
+        [[nodiscard]] SLuint32 GetPlayState() const;
+
         void enqueueBuffer();
+
+        void onData(bytes::unique_binary data) override;
 
     public:
         OpenSLESDeviceModule(const AudioDescription* desc, bool isCapture, BaseSink* sink);
