0251d7b5b1d805100be5caa6a234487aa80449e6
Laky64 <iraci.matteo@gmail.com>
2024-10-09 19:54:10 +0200

    Fixed Windows std::clamp with algorithm

--- ntgcalls/src/io/audio_mixer.cpp
+++ ntgcalls/src/io/audio_mixer.cpp
@@ -2,17 +2,19 @@
 // Created by Laky64 on 07/10/24.
 //
 
+#include <algorithm>
 #include <ntgcalls/io/audio_mixer.hpp>
 
 namespace ntgcalls {
     AudioMixer::AudioMixer(BaseSink* sink): AudioWriter(sink) {}
 
     void AudioMixer::sendFrames(const std::map<uint32_t, bytes::unique_binary>& frames) {
-        bytes::unique_binary mixedOutput = bytes::make_unique_binary(sink->frameSize());
-        std::fill_n(reinterpret_cast<int16_t*>(mixedOutput.get()), sink->frameSize() / sizeof(int16_t), 0);
+        const auto frameSize = sink->frameSize();
+        bytes::unique_binary mixedOutput = bytes::make_unique_binary(frameSize);
+        std::fill_n(reinterpret_cast<int16_t*>(mixedOutput.get()), frameSize / sizeof(int16_t), 0);
 
         const auto numSources = frames.size();
-        for (size_t i = 0; i < sink->frameSize() / sizeof(int16_t); i++) {
+        for (size_t i = 0; i < frameSize / sizeof(int16_t); i++) {
             int32_t mixedSample = 0;
             // ReSharper disable once CppUseElementsView
             for (const auto& [fst, snd] : frames) {
@@ -25,7 +27,7 @@ namespace ntgcalls {
 
             // Clipping to 16-bit signed integer range
             const auto mixedOutputSamples = reinterpret_cast<int16_t*>(mixedOutput.get());
-            mixedOutputSamples[i] = static_cast<int16_t>(std::clamp(mixedSample, INT16_MIN, INT16_MAX));
+            mixedOutputSamples[i] = static_cast<int16_t>(std::clamp(mixedSample, INT16_MIN, static_cast<int32_t>(INT16_MAX)));
         }
 
         onData(std::move(mixedOutput));
