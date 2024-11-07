c0c86f9784381e8c50b641fa39ec0b878a2e6e82
Laky64 <iraci.matteo@gmail.com>
2024-11-07 15:09:06 +0100

    Fixed OpenH264 message status

--- cmake/FindOpenH264.cmake
+++ cmake/FindOpenH264.cmake
@@ -1,13 +1,9 @@
 set(OPENH264_DIR ${deps_loc}/openh264)
-set(OPENH264_DOWNLOAD ${OPENH264_DIR}/download)
 set(OPENH264_SRC ${OPENH264_DIR}/src)
 set(OPENH264_GIT https://github.com/pytgcalls/openh264)
 set(OPENH264_LIB ${CMAKE_STATIC_LIBRARY_PREFIX}openh264${CMAKE_STATIC_LIBRARY_SUFFIX})
 
 if(NOT TARGET cisco::OpenH264)
-    GetProperty("version.openh264" OPENH264_VERSION)
-    message(STATUS "openh264 v${OPENH264_VERSION}")
-
     if (LINUX_ARM64)
         set(PLATFORM linux)
         set(ARCHIVE_FORMAT .tar.gz)
@@ -28,6 +24,9 @@ if(NOT TARGET cisco::OpenH264)
         message(STATUS "OpenH264 is not supported on ${CMAKE_SYSTEM_NAME} with ${CMAKE_HOST_SYSTEM_PROCESSOR}")
         return()
     endif ()
+
+    GetProperty("version.openh264" OPENH264_VERSION)
+    message(STATUS "openh264 v${OPENH264_VERSION}")
 
     set(FILE_NAME openh264.${PLATFORM}-${ARCH}${ARCHIVE_FORMAT})
 
