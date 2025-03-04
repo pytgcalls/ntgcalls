e0217b622d2f14b7dc1bb4f15b1dc6e2300b4bce
Laky64 <iraci.matteo@gmail.com>
2025-03-04 20:29:36 +0100

    Fixed Frame class

--- android/app/src/main/java/io/github/pytgcalls/media/Frame.java
+++ android/app/src/main/java/io/github/pytgcalls/media/Frame.java
@@ -1,9 +1,9 @@
 package io.github.pytgcalls.media;
 
 public class Frame {
-    long ssrc;
-    byte[] data;
-    FrameData frameData;
+    public final long ssrc;
+    public final byte[] data;
+    public final FrameData frameData;
 
     Frame(long ssrc, byte[] data, FrameData frameData) {
         this.ssrc = ssrc;
