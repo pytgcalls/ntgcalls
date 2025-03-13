ef097af8d843fdcc526ce7745f7a9a47564911f4
Laky64 <iraci.matteo@gmail.com>
2025-03-13 19:40:41 +0100

    Fixed capture timestamp

--- ntgcalls/src/stream_manager.cpp
+++ ntgcalls/src/stream_manager.cpp
@@ -300,7 +300,6 @@ namespace ntgcalls {
                     syncReaders.insert(device);
                     std::weak_ptr weak(shared_from_this());
                     readers[device]->onData([weak, id, streamType, isShared](const bytes::unique_binary& data, wrtc::FrameData frameData) {
-                        frameData.absoluteCaptureTimestampMs = rtc::TimeMillis();
                         const auto strong = weak.lock();
                         if (!strong) {
                             return;
@@ -319,6 +318,7 @@ namespace ntgcalls {
                         }
                         if (strong->streams.contains(id)) {
                             if (const auto stream = dynamic_cast<BaseStreamer*>(strong->streams[id].get())) {
+                                frameData.absoluteCaptureTimestampMs = rtc::TimeMillis();
                                 if (streamType == Video && isShared) {
                                     (void) strong->framesCallback(
                                         id.first,
