1431ff567a17db72f66536bcdb1cd73c3e6ec96a
Laky64 <iraci.matteo@gmail.com>
2025-04-02 23:20:09 +0200

    Fixed GLIBC Compatibilty for Non-Linux OSes

--- examples/go/glibc_compatibility.h
+++ examples/go/glibc_compatibility.h
@@ -1,9 +1,9 @@
-#include <resolv.h>
-
 // This header file provides compatibility for the glibc >= 2.28 version for _dn_expand and __res_nquery functions.
+
 #ifdef __GLIBC__
-	#if __GLIBC__ > 2 || (__GLIBC__ == 2 && __GLIBC_MINOR__ >= 28)
-		int __dn_expand(const unsigned char *src, const unsigned char *src_end,
+    #if __GLIBC__ > 2 || (__GLIBC__ == 2 && __GLIBC_MINOR__ >= 28)
+		#include <resolv.h>
+        int __dn_expand(const unsigned char *src, const unsigned char *src_end,
 		                 unsigned char *dst, int dst_len, int options) {
 		    int n = res_query((char *)src, C_IN, T_PTR, dst, dst_len);
 		    if (n < 0) {
