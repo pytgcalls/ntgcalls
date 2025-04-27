26bc10b1d5beefeeef3f1a434d2afec5722fc52e
Laky64 <iraci.matteo@gmail.com>
2025-04-28 00:11:48 +0200

    Fixed missing libs in go example for macOS

--- examples/go/main.go
+++ examples/go/main.go
@@ -5,7 +5,7 @@ package main
 // but you need to change the name of the library in the LDFLAGS and the -L folder from . to the
 // folder where the library is located.
 #cgo linux LDFLAGS: -L . -lntgcalls -lm -lz
-#cgo darwin LDFLAGS: -L . -lntgcalls -lc++ -liconv -framework AVFoundation -framework AudioToolbox -framework CoreAudio -framework QuartzCore -framework CoreMedia -framework VideoToolbox -framework AppKit -framework Metal -framework MetalKit -framework OpenGL -framework IOSurface -framework ScreenCaptureKit
+#cgo darwin LDFLAGS: -L . -lntgcalls -lc++ -lz -lbz2 -liconv -framework AVFoundation -framework AudioToolbox -framework CoreAudio -framework QuartzCore -framework CoreMedia -framework VideoToolbox -framework AppKit -framework Metal -framework MetalKit -framework OpenGL -framework IOSurface -framework ScreenCaptureKit
 
 // Currently is supported only dynamically linked library on Windows due to
 // https://github.com/golang/go/issues/63903
