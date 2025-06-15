5a1d4785df98901b8ae07868c29ecf02b599813b
Laky64 <iraci.matteo@gmail.com>
2025-06-15 03:43:37 +0200

    Use External NDK Config

--- cmake/FindNDK.cmake
+++ cmake/FindNDK.cmake
@@ -2,25 +2,32 @@ if (ANDROID_ABI)
     set(NDK_DIR ${DEPS_DIR}/ndk)
     set(NDK_SRC ${NDK_DIR}/src)
 
-    GetProperty("version.ndk" NDK_VERSION)
-    message(STATUS "android-ndk ${NDK_VERSION}")
-    if (NOT EXISTS ${NDK_SRC})
-        if (NOT LINUX)
-            message(FATAL_ERROR "Only Linux is supported for Android build")
+    if (NOT ANDROID_NDK)
+        GetProperty("version.ndk" NDK_VERSION)
+        message(STATUS "android-ndk ${NDK_VERSION}")
+        if (NOT EXISTS ${NDK_SRC})
+            if (NOT LINUX)
+                message(FATAL_ERROR "Only Linux is supported for Android build")
+            endif ()
+            DownloadProject(
+                URL https://dl.google.com/android/repository/android-ndk-${NDK_VERSION}-linux.zip
+                DOWNLOAD_DIR ${NDK_DIR}/download
+                SOURCE_DIR ${NDK_SRC}
+            )
         endif ()
-        DownloadProject(
-            URL https://dl.google.com/android/repository/android-ndk-${NDK_VERSION}-linux.zip
-            DOWNLOAD_DIR ${NDK_DIR}/download
-            SOURCE_DIR ${NDK_SRC}
-        )
+        GetProperty("version.sdk_compile" ANDROID_NATIVE_API_LEVEL)
     endif ()
-    GetProperty("version.sdk_compile" ANDROID_NATIVE_API_LEVEL)
+
     set(ANDROID_NATIVE_API_LEVEL    ${ANDROID_NATIVE_API_LEVEL})
     set(ANDROID_PLATFORM            ${ANDROID_NATIVE_API_LEVEL})
     set(ANDROID_STL                 none)
     set(ANDROID_CPP_FEATURES        exceptions rtti)
     set(CMAKE_ANDROID_EXCEPTIONS    ON)
-    set(ANDROID_NDK                 OFF)
+
+    if (NOT ANDROID_NDK)
+        set(ANDROID_NDK OFF)
+    endif ()
+
     include(${NDK_SRC}/build/cmake/android.toolchain.cmake)
     set(ANDROID_CLANG_LIBRARY_FOLDER "${ANDROID_TOOLCHAIN_ROOT}/lib/clang/")
     file(GLOB _clang_root_dirs "${ANDROID_CLANG_LIBRARY_FOLDER}*")
