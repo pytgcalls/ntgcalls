a9c386eeb1f2641819a5af83760f93383c7f0699
Laky64 <iraci.matteo@gmail.com>
2025-11-01 19:04:26 +0100

    Fixed Python 3.14 Development Module

--- cmake/Toolchain.cmake
+++ cmake/Toolchain.cmake
@@ -6,7 +6,7 @@ set(NTGCALLS_TOOLCHAIN_INCLUDED ON)
 get_filename_component(DEPS_DIR "${CMAKE_CURRENT_LIST_DIR}/../deps" REALPATH)
 get_filename_component(props_loc "${CMAKE_CURRENT_LIST_DIR}/../version.properties" REALPATH)
 
-find_package(Python3 REQUIRED COMPONENTS Interpreter QUIET)
+find_package(Python3 REQUIRED COMPONENTS Interpreter Development.Module QUIET)
 
 include(${CMAKE_CURRENT_LIST_DIR}/PlatformUtils.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/FindCURL.cmake)
